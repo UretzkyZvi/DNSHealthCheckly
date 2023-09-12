35c7b45b30b4ad8a456864e34923a0c691f34513
greguretzky <uretsky.zvi@gmail.com>
2023-09-12 16:29:51 +0200

    Implement data retrieval API endpoints for DNS metrics and server health.

--- src/server.js
+++ src/server.js
@@ -1,7 +1,18 @@
 const express = require("express");
+const MetricsStorage = require("./lib/MetricsStorage");
+const { Client } = require("pg");
+
+const dbClient = new Client({
+  host: "postgres", // docker-compose.yml service name
+  port: 5432,
+  user: "postgres",
+  password: "P@ssw0rd",
+  database: "dnshealthcheckly",
+});
+
 const app = express();
 const port = 3000;
-
+const metricsStorage = new MetricsStorage(dbClient);
 const userSettings = {
   domain: "www.google.com",
   region: "Global",
@@ -26,6 +37,33 @@ app.post("/config", (req, res) => {
   // Here, add some validation for incoming settings if needed
   Object.assign(userSettings, req.body);
   res.json({ message: "Configuration updated." });
+});
+
+// Endpoint to get DNS metrics
+app.get("/metrics", async (req, res) => {
+  try {
+    const metrics = await metricsStorage.getMetrics(req.query);
+    res.json(metrics);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Failed to retrieve metrics", error: error.message });
+  }
+});
+
+// Endpoint to get server health
+app.get("/server-health", async (req, res) => {
+  try {
+    const healthData = await metricsStorage.getServerHealth(req.query);
+    res.json(healthData);
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        message: "Failed to retrieve server health",
+        error: error.message,
+      });
+  }
 });
 
 app.listen(port, () => {
